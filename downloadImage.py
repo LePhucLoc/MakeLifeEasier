11bc09ade6241611827377e46bf1ad612d0227a8
Lude <loclion17@gmail.com>
2019-10-20 10:23:57 +0700

    first commit

--- downloadImage.py
+++ downloadImage.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"hentai","limit":10,"print_urls":True}   #creating list of arguments
+arguments = {"keywords":"nancy","limit":10,"print_urls":True}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)
